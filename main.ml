0b83c33806cde36b387c75efb0ea698b75374e0d
Myunghun Cha <scorpiox.kr@gmail.com>
2012-08-09 21:34:21 +0900

    Fixed Makefile minor bug

--- main.ml
+++ main.ml
@@ -1,9 +1,21 @@
-open Types;;
+open Lexer
+open Types
+open String
+open Buffer
 
 let main() =
+  let lexbuf = Lexing.from_channel stdin in
+  let read () = Parser.pmain minilisp lexbuf in
+  let output = Buffer.create 0 in
 
-  print_endline "Not implemented yet";
-  
+  try
+    while true do
+      let s_exp = read () in
+      print_endline (string_of_s_exp s_exp)
+    done;
+  with
+    Lexer.Eof -> print_string (Buffer.contents output)
+  | _ -> exit 1;
 ;;
 
 main();;
